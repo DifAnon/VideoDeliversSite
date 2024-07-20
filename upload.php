026e9ac23bc11f4f5b725a629a36222277fc7c1e
DifAnon <142886715+DifAnon@users.noreply.github.com>
2024-07-20 18:39:28 +0300

    Update upload.php
    Добавление комментариев

--- upload.php
+++ upload.php
@@ -1,15 +1,20 @@
 <?php
 
+#Подключение CSS
 echo "<link rel='stylesheet' href='css/style2.css'>";
 
+#Определение URL-адреса сайта
 $url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'];
 
+#Изменение имени на уникальный id
 $FileName = $_FILES['videoF']['name'] = uniqid(12). '' .'.mp4' ;
-$fileRealName = $_FILES['videoF']['name'];
 
+#Папка, куда сохраняются видео
 $uploaddir = 'vid/';
+
 $uploadfile = $uploaddir . $FileName;
 
+#Максимальный размер видео, которе можно загрузить
 ini_set('post_max_size', '20M');
 ini_set('upload_max_filesize', '20M');
 
